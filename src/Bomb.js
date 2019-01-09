c1cda2dbd32dc1495eb46c0b60156b96f3aa883f
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-01-09 13:32:33 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,12 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+  render() {
+    return this.props.initialCount === 0 ? (
+      <div>Boom!</div>
+    ) : (
+      <div>{this.props.initialCount} seconds left before I go boom!</div>
+    );
+  }
+}
